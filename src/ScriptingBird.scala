856b8b8e647302ab744c8265a08c6d6e63b40892
David Morand <david.morand@nuecho.com>
2010-10-27 22:23:01 -0400

    replaced verifyCredentials by getMentions (not working because of authentification)

--- src/ScriptingBird.scala
+++ src/ScriptingBird.scala
@@ -7,22 +7,20 @@ object ScriptingBird {
   def main(args: Array[String]): Unit = {
     println("Password: ")
     val password = new BufferedReader(new InputStreamReader(System.in)).readLine()
-    println(new ScriptingBird(password).verifyCredentials)
+    println(new ScriptingBird(password).getMentions)
   }
 }
 
 class ScriptingBird(password: String) {
-  def verifyCredentials: Boolean = {
+  def getMentions: String = {
     val client = new HttpClient()
-    val method = new GetMethod("http://twitter.com/help/test.xml")
+    val method = new GetMethod("http://twitter.com/statuses/mentions.xml")
     val credentials = new UsernamePasswordCredentials("scriptingbird", password)
 
     method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false))
     client.getParams().setAuthenticationPreemptive(true)
     client.getState().setCredentials(new AuthScope("twitter.com", 80, AuthScope.ANY_REALM), credentials)
     client.executeMethod(method)
-
-    val statusLine = method.getStatusLine()
-    statusLine.getStatusCode() == 200
+    method.getResponseBodyAsString()
   }
 }
